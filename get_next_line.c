2dc18234fb88d163a661cec54f4614657ebc300f
Sahra Taskinen <85313678+staskine@users.noreply.github.com>
2022-03-29 14:42:10 +0300

    Update get_next_line.c
    Hopefully my final version

--- get_next_line.c
+++ get_next_line.c
@@ -11,31 +11,30 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 static int	get_result(const int fd, char **string, char **line)
 {
 	int		index;
-	char	*temp;
+	char	*tmp;
 
 	index = 0;
 	if (string[fd][0] == '\0')
 		return (0);
-	while (string[fd][index]!= '\n' && string[fd][index] != '\0')
+	while (string[fd][index] != '\n' && string[fd][index] != '\0')
 		index++;
-	//printf("%d, index", index);
 	if (string[fd][index] == '\0')
 	{
 		*line = ft_strdup(string[fd]);
-		free(string[fd]);
+		ft_strdel(&string[fd]);
 		return (1);
 	}
 	else
 	{
 		*line = ft_strsub(string[fd], 0, index);
-		temp = ft_strdup(string[fd]);
-		free(string[fd]);
-		string[fd] = ft_strsub(temp, (index + 1), (ft_strlen(temp) - index));
+		tmp = ft_strdup(string[fd]);
+		ft_strdel(&string[fd]);
+		string[fd] = ft_strsub(tmp, (index + 1), (ft_strlen(tmp) - index - 1));
+		ft_strdel(&tmp);
 		return (1);
 	}
 }
@@ -50,38 +49,47 @@ static int	check_result(int ret, const int fd, char **string, char **line)
 		return (get_result(fd, string, line));
 }
 
+static int	strcompile(char **string, char *buf, int fd)
+{
+	char	*temp;
+
+	if (string[fd] == NULL)
+	{
+		string[fd] = ft_strdup(buf);
+		if (string[fd] == NULL)
+			return (1);
+	}
+	else
+	{
+		temp = ft_strjoin(string[fd], buf);
+		if (temp == NULL)
+			return (1);
+		ft_strdel(&string[fd]);
+		string[fd] = ft_strdup(temp);
+		ft_strdel(&temp);
+	}
+	return (0);
+}
+
 int	get_next_line(const int fd, char **line)
 {
 	static char	*string[FD_SIZE];
 	char		buf[BUFF_SIZE + 1];
-	char		*temp;
 	int			ret;
 
 	if (fd < 0 || line == NULL || fd > FD_SIZE)
 		return (-1);
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	ret = read(fd, buf, BUFF_SIZE);
+	if (ret < 0)
+		return (-1);
+	while (ret > 0)
 	{
 		buf[ret] = '\0';
-		//printf("%s, buf\n", buf);
-		if (string[fd] == NULL)
-		{
-			string[fd] = ft_strdup(buf);
-			if (string[fd] == NULL)
-				return (-1);
-			//printf("%s, first if\n", string[fd]);
-		}
-		else
-		{
-			temp = ft_strjoin(string[fd], buf);
-			if (temp == NULL)
-				return (-1);
-			string[fd] = ft_strdup(temp);
-			free(temp);
-			//printf("%s, string[fd] joining\n", string[fd]);
-		}
+		if ((strcompile(string, buf, fd)) == 1)
+			return (-1);
 		if (ft_strchr(string[fd], '\n'))
-			break;
+			break ;
+		ret = read(fd, buf, BUFF_SIZE);
 	}
-	//printf("%d", ret);
 	return (check_result(ret, fd, string, line));
 }
