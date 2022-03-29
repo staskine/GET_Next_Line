bfe196eb0129d8213fceed82147ffb246f35fbda
Sahra Taskinen <85313678+staskine@users.noreply.github.com>
2022-03-29 14:43:21 +0300

    Create get_next_line.h
    The h file for the project

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: staskine <staskine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/05 12:43:55 by staskine          #+#    #+#             */
+/*   Updated: 2022/03/03 12:20:10 by staskine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+
+# define BUFF_SIZE 8
+# define FD_SIZE 4096
+
+int	get_next_line(const int fd, char **line);
+
+#endif
